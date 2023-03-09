f19b729f01cc3ae83f464b61ea504790f0f437da
Tony Wong <wongtony@vmware.com>
2023-03-09 13:22:31 -0500

    Update message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + Team!";
+		return "Greetings from Spring Boot + Tanzu + Everybody!";
 	}
 
 }
